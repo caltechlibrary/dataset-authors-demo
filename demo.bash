24d24feada5b3da84816220497104cec3e320e14
R. S. Doiel <rsdoiel@gmail.com>
2017-06-13 15:28:26 -0700

    removed CR question between sections of demo

--- demo.bash
+++ demo.bash
@@ -13,7 +13,6 @@ if [ -d caltechdata.bleve ]; then
 fi
 
 echo "Run dataset command and create our collection from our CaltechAUTHORS sample"
-read -p "Press any key to run command, ctrl-C to exit" NEXT
 $(dataset init caltechauthors)
 for ITEM in $(ls authors/*.json); do
     ID=$(jsoncols -i "${ITEM}" .id)
@@ -22,21 +21,19 @@ for ITEM in $(ls authors/*.json); do
 done
 
 echo "Grab records from CaltechDATA and create collection"
-read -p "Press any key to run command, ctrl-C to exit" NEXT
 $(dataset init caltechdata)
 python3 caltechdata_feeds.py
 
 echo ""
 echo "Run dsindexer to index our collections based on our definition in caltechauthors.json and caltechdata.json"
-read -p "Press any key to run command, ctrl-C to exit" NEXT
 dsindexer -c caltechauthors caltechauthors.json
 dsindexer -c caltechdata caltechdata.json
 
 echo ""
 echo "Run dsws for a web searchable version of our collection"
-read -p "Press any key to run command, ctrl-C to exit" NEXT
 echo ""
 echo "Open your web browser and go to http://localhost:8011"
+echo ""
 echo ""
 dsws -dev-mode=true -t templates caltechauthors.bleve caltechdata.bleve
 
